05f0c35029836543b4cbb9f8ae9e96d0e5a15dd9
hyeonkkyu <milktea185ml@naver.com>
2022-10-12 17:43:32 +0900

    22.10.12

--- src/pages/SignUp copy.js
+++ src/pages/SignUp copy.js
@@ -1,38 +1,53 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import '../css/signup.css';
 
 const SignUp = () => {
+  const [inputs, setInputs] = useState({
+    name: '',
+    nickname: ''
+  });
+  const nameInput = useRef();
 
-  
+  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
+
+  const onChange = e => {
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value // name 키를 가진 값을 value 로 설정
+    });
+  };
+
+  const onReset = () => {
+    setInputs({
+      name: '',
+      nickname: ''
+    });
+    nameInput.current.focus();
+  };
 
   return (
-    <section className='SignUp inner'>
-      hi!!!
-      <h2>SIGNUP</h2>
-      <form action="" method='POST'>
-        <p>아이디</p>
-        <div className='inputBox'>
-          <input id="user_id" type="text" name="user_id" placeholder="아이디" />
-          <label for="user_id">아이디</label>
-        </div>
-        <p>비밀번호</p>
-        <div className='inputBox'>
-          <input id="user_pw" type="text" name="user_pw" placeholder="비밀번호" />
-          <label for="user_pw">비밀번호</label>
-        </div>
-        <p>비밀번호 확인</p>
-        <div className='inputBox'>
-          <input id="user_pw" type="text" name="user_pw" placeholder="비밀번호" />
-          <label for="user_pw">비밀번호</label>
-        </div>
-        <input type="submit" value="로그인" />
-
-        {/* <input onChange={handlerInput} name='title' value={todo.title || ''} ref={ipref} />
-            <input onChange={handlerInput} name='content' value={todo.content || ''} />
-            <button onClick={handlerList}>WRITE</button> */}
-      </form>
-    </section>
-  )
+    <div>
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
+      <button onClick={onReset}>초기화</button>
+      <div>
+        <b>값: </b>
+        {name} ({nickname})
+      </div>
+    </div>
+  );
 }
 
 export default SignUp
